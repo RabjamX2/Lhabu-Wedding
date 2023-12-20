d038236801e0aead91669c6ed9d59133bc67e7d1
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2023-12-19 21:24:20 -0500

    dynamic name

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/<name>")
+def index(name):
+    formatted_name = name.replace("+", " ")
+    return render_template("index.html", name=formatted_name)
 
 
 if __name__ == "__main__":
