17c0e33ace0358f5531d210dd4e58544bd462a8c
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2023-12-19 21:35:30 -0500

    fix

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/<name>")
-def index(name):
+def indexName(name):
     formatted_name = name.replace("+", " ")
     return render_template("index.html", name=formatted_name)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
