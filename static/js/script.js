d823ea4d75513d0857bba6b188cc2b75c531d2da
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2023-12-19 23:29:41 -0500

    fix

--- static/js/script.js
+++ static/js/script.js
@@ -20,16 +20,16 @@ images.forEach((img, index) => {
 
 function adjustFontSize() {
 	const container = document.getElementById("landing-header");
-	let fontSize = 20; // Start with a default font size
+	let fontSize = 100; // Start with a default font size
 	container.style.fontSize = fontSize + "px";
 
-	// Reduce the font size until the text fits in one line
-	while (container.scrollWidth > container.offsetWidth) {
+	// Reduce the font size until the text fits within the width and height of the container
+	while (container.scrollWidth > container.offsetWidth || container.scrollHeight > container.offsetHeight) {
 		fontSize--;
+		if (fontSize <= 0) break; // Prevents font size from becoming negative
 		container.style.fontSize = fontSize + "px";
 	}
 }
 
-// Call this function whenever the text changes and on window resize
 window.onresize = adjustFontSize;
 adjustFontSize();
