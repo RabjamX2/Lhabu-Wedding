ea374467fd7f689bd123c2ca6d6e16681da15f51
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2023-12-22 21:03:38 -0500

    new page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
 
@@ -12,6 +13,28 @@ def index():
 def indexName(name):
     formatted_name = name.replace("+", " ")
     return render_template("index.html", name=formatted_name)
+
+
+@app.route("/invite/<name>")
+def inviteName(name):
+    formatted_name = name.replace("+", " ")
+    image = Image.open("static/images/Blank Name.png")
+    draw = ImageDraw.Draw(image)
+    biggestTextSize = 190
+    font = ImageFont.truetype("static/fonts/Blacksword.otf", biggestTextSize)
+
+    # Calculate text width and height
+    text_width = draw.textlength(formatted_name, font=font)
+    while text_width > 1550:
+        biggestTextSize -= 10
+        font = ImageFont.truetype("static/fonts/Blacksword.otf", biggestTextSize)
+        text_width = draw.textlength(formatted_name, font=font)
+
+    x = 3723 - (text_width / 2)  # Center the X coordinate
+    y = 2635
+
+    draw.text((x, y), formatted_name, fill="white", font=font)
+    return send_file("modified_image.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
